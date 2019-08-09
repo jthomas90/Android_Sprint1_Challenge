df7773bb94815c7ed01f68da2302d286d315b29c
Janiece Thomas <janiece.t90@gmail.com>
2019-08-09 13:52:36 -0400

    Created wrong original files so hat to manually go in and male oncreates

--- app/src/main/java/com/example/android_sprint1_challenge/EditPage.kt
+++ app/src/main/java/com/example/android_sprint1_challenge/EditPage.kt
@@ -1,12 +1,26 @@
 package com.example.android_sprint1_challenge
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class EditPage : AppCompatActivity() {
+    companion object {
+        const val MOVIE_REQUEST_CODE = 12
+    }
+
+        //private var movieData = MovieData()
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_page2)
+        }
 
 
 
 
 
 
-}
+
+
+    }
+
